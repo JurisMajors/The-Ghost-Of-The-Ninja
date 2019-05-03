4dc791502438c820cabd4c266b59c2e62776b5df
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-03 23:43:04 +0200

    Ready for commenting

--- src/group4/graphics/Shader.java
+++ src/group4/graphics/Shader.java
@@ -4,11 +4,26 @@ import static org.lwjgl.opengl.GL41.*;
 
 public class Shader {
     private int shaderId;
+    private int vertShader;
+    private int fragShader;
 
     public Shader(String vertSource, String fragSource) {
+        // Create the shader program
         shaderId = glCreateProgram();
-        int vertShader = glCreateShader(GL_VERTEX_SHADER);
-        int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+        // Create the (sub) shaders
+        vertShader = compileShader(GL_VERTEX_SHADER, vertSource);
+        fragShader = compileShader(GL_FRAGMENT_SHADER, fragSource);
+
+        // Actually load the shaders into the GPU
+        glAttachShader(shaderId, vertShader);
+        glAttachShader(shaderId, fragShader);
+        glLinkProgram(shaderId);
+        glValidateProgram(shaderId);
+    }
+
+    public int getShaderId() {
+        return this.shaderId;
     }
 
     void bind() {
@@ -19,7 +34,10 @@ public class Shader {
         glUseProgram(0);
     }
 
+    // TODO: setUniform functions. Which we currently might not even need.
+
     int getUniformLocation(String name) {
+        // TODO: Cache the locations if we start doing this call often.
         int location = glGetUniformLocation(shaderId, name);
         if (location == -1) {
             System.err.printf("[WARNING] Uniform location %s does not exist in Shader %d \n", name, shaderId);
@@ -27,7 +45,18 @@ public class Shader {
         return location;
     }
 
-    private long compileShader() {
-        glCreateShader(GL_VERTEX_SHADER, vertexSource);
+    private int compileShader(int type, String source) {
+        int id = glCreateShader(type);
+        glShaderSource(id, source);
+        glCompileShader(id);
+
+        int[] result = new int[1];
+        glGetShaderiv(id, GL_COMPILE_STATUS, result);
+        if (result[0] == GL_FALSE) {
+            System.err.printf("Could not compile %s\n", type);
+            System.err.println(glGetShaderInfoLog(id));
+            id = 0; // 0 so we can graciously fail and keep running.
+        }
+        return id;
     }
 }
