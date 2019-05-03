d7035cdaac8f3052b4d705ad4d8cdef0359aaed1
JurisMajors <j.a.majors@student.tue.nl>
2019-05-03 19:54:26 +0200

    extends to implements

--- src/group4/AI/Ghost.java
+++ src/group4/AI/Ghost.java
@@ -3,7 +3,7 @@ package group4.AI;
 /**
  * The AI Player, passed to the game package.
  */
-public class Ghost { //TODO: extends Player
+public class Ghost { //TODO: implements Player
     Brain brain;
 
     Ghost(Brain b) {
