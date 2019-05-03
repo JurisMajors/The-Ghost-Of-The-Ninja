645cf5ff42f7b87b9c093c9ea1da2c179a4fb97f
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-03 23:53:19 +0200

    Comments

--- src/group4/graphics/Shader.java
+++ src/group4/graphics/Shader.java
@@ -3,6 +3,7 @@ package group4.graphics;
 import static org.lwjgl.opengl.GL41.*;
 
 public class Shader {
+    // For storing the program ids required for a shader.
     private int shaderId;
     private int vertShader;
     private int fragShader;
@@ -22,21 +23,37 @@ public class Shader {
         glValidateProgram(shaderId);
     }
 
+    /**
+     * Gives the id of the shader program.
+     * @return int, the id.
+     */
     public int getShaderId() {
         return this.shaderId;
     }
 
-    void bind() {
+    /**
+     * Sets the shader as the active shader to render with on the GPU.
+     */
+    public void bind() {
         glUseProgram(shaderId);
     }
 
-    void unbind() {
+    /**
+     * Sets the default shader (0) as the active shader to render with on the GPU.
+     */
+    public void unbind() {
         glUseProgram(0);
     }
 
     // TODO: setUniform functions. Which we currently might not even need.
 
-    int getUniformLocation(String name) {
+    /**
+     * Finds the specific location of a given uniform within the shader based on its name.
+     *
+     * @param name String, the name of the uniform to look up.
+     * @return int, location of the uniform within the shader. -1 if uniform does not exist.
+     */
+    private int getUniformLocation(String name) {
         // TODO: Cache the locations if we start doing this call often.
         int location = glGetUniformLocation(shaderId, name);
         if (location == -1) {
@@ -45,13 +62,24 @@ public class Shader {
         return location;
     }
 
+    /**
+     * Given a shader type and the source code of the shader, this function creates and compiles
+     * the appropriate shader for use in OpenGL.
+     *
+     * @param type (GL_VERTEX_SHADER | GL_FRAGMENT_SHADER)
+     * @param source String with the source code of the shader to be created
+     * @return id of shader if compiled successfully, 0 if compilation failed.
+     */
     private int compileShader(int type, String source) {
-        int id = glCreateShader(type);
-        glShaderSource(id, source);
-        glCompileShader(id);
+        int id = glCreateShader(type);  // Create a default shader of given type
+        glShaderSource(id, source);     // Overwrite the sourcecode of the shader
+        glCompileShader(id);            // Compile the newly written sourcecode
 
-        int[] result = new int[1];
+        // Get the compilation status.
+        int[] result = new int[1];  // sic.
         glGetShaderiv(id, GL_COMPILE_STATUS, result);
+
+        // If compilation failed...
         if (result[0] == GL_FALSE) {
             System.err.printf("Could not compile %s\n", type);
             System.err.println(glGetShaderInfoLog(id));
