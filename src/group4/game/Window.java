23fdfc95484bb75a46c4537b655fdb282022bcf4
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-03 13:21:24 +0200

    Copy over Main and start Window class

---
+++ src/group4/game/Window.java
@@ -0,0 +1,35 @@
+import org.lwjgl.glfw.GLFWVidMode;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Window {
+    private long window;
+    private int width = 1280;
+    private int height = 720;
+
+    public Window() {
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the actual window
+        window = glfwCreateWindow(width, height, "Platformer", NULL, NULL);
+        if (window == NULL) {
+            throw new IllegalStateException("Window is NULL");
+        }
+
+        // Positioning the window in the center
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2);
+
+        // Setting the context and showing
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
+    }
+
+    public void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
