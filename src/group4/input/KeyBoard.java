48ec76663732675c29e153102b1cce287474b0a5
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-05-03 22:28:05 +0200

    Added mouse input, implemented all input listeners into the main class

--- src/group4/input/KeyBoard.java
+++ src/group4/input/KeyBoard.java
@@ -1,19 +1,26 @@
 package group4.input;
 
-import static org.lwjgl.glfw.GLFW.*;
 import org.lwjgl.glfw.GLFWKeyCallback;
 
-public class Input extends GLFWKeyCallback {
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
-    // true means this key is currently down
+public class KeyBoard extends GLFWKeyCallback {
+
+    // True means the key at this position is currently down
     public static boolean[] keys = new boolean[65536];
 
+    /**
+     * Gets called when input is received from the keyboard.
+     */
     @Override
     public void invoke(long window, int key, int scancode, int action, int mods) {
         // only turn on if the button is not released
         keys[key] = action != GLFW_RELEASE;
     }
 
+    /**
+     * Returns true if key is down.
+     */
     public static boolean isKeyDown(int key) {
         return keys[key];
     }
