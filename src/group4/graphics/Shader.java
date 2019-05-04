e595eecfcda80dbfb3869a820b54406ae8f3647a
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-04 21:27:40 +0200

    Removed the unbind method and added more comments

--- src/group4/graphics/Shader.java
+++ src/group4/graphics/Shader.java
@@ -9,13 +9,14 @@ import java.util.Map;
 import static org.lwjgl.opengl.GL41.*;
 
 public class Shader {
+    // Storing the attribute locations for in the shader
     public static final int VERTEX_ATTRIBUTE = 0;
     public static final int TEXCOORD_ATTRIBUTE = 1;
 
-    // For storing the program ids required for a shader.
+    // For storing the program id required for a shader.
     private int shaderId;
-    private int vertShader;
-    private int fragShader;
+
+    // For remembering the locations of uniforms within the shaders
     private Map<String, Integer> uniformLocationCache = new HashMap<>();
 
     public Shader(String vertSource, String fragSource) {
@@ -23,8 +24,8 @@ public class Shader {
         shaderId = glCreateProgram();
 
         // Create the (sub) shaders
-        vertShader = compileShader(GL_VERTEX_SHADER, vertSource);
-        fragShader = compileShader(GL_FRAGMENT_SHADER, fragSource);
+        int vertShader = compileShader(GL_VERTEX_SHADER, vertSource);
+        int fragShader = compileShader(GL_FRAGMENT_SHADER, fragSource);
 
         // Actually load the shaders into the GPU
         glAttachShader(shaderId, vertShader);
@@ -49,17 +50,10 @@ public class Shader {
     }
 
     /**
-     * Sets the default shader (0) as the active shader to render with on the GPU.
-     */
-    public void unbind() {
-        glUseProgram(0);
-    }
-
-    /**
      * Finds the specific location of a given uniform within the shader based on its name.
      *
      * @param name String, the name of the uniform to look up.
-     * @return int, location of the uniform within the shader. -1 if uniform does not exist.
+     * @return Integer, location of the uniform within the shader. -1 if uniform does not exist.
      */
     private int getUniformLocation(String name) {
         if (uniformLocationCache.containsKey(name)) {
@@ -74,26 +68,57 @@ public class Shader {
         return location;
     }
 
+    /**
+     * Sets the uniform with the given name to the given value.
+     *
+     * @param name String, denoting the uniform
+     * @param value Integer, value to set
+     */
     public void setUniform1i(String name, int value) {
         int location = getUniformLocation(name);
         glUniform1i(location, value);
     }
 
+    /**
+     * Sets the uniform with the given name to the given value.
+     *
+     * @param name String, denoting the uniform
+     * @param value Float, value to set
+     */
     public void setUniform1f(String name, float value) {
         int location = getUniformLocation(name);
         glUniform1f(location, value);
     }
 
+    /**
+     * Sets the uniform with the given name to the given value.
+     *
+     * @param name String, denoting the uniform
+     * @param v1 Float, first value to set
+     * @param v2 Float, second value to set
+     */
     public void setUniform2f(String name, float v1, float v2) {
         int location = getUniformLocation(name);
         glUniform2f(location, v1, v2);
     }
 
+    /**
+     * Sets the uniform with the given name to the given value.
+     *
+     * @param name String, denoting the uniform
+     * @param vector Vector3f, value to set
+     */
     public void setUniform3f(String name, Vector3f vector) {
         int location = getUniformLocation(name);
         glUniform3f(location, vector.x, vector.y, vector.z);
     }
 
+    /**
+     * Sets the uniform with the given name to the given value.
+     *
+     * @param name String, denoting the uniform
+     * @param matrix Matrix4f, value to set
+     */
     public void setUniformMat4f(String name, Matrix4f matrix) {
         int location = getUniformLocation(name);
         glUniformMatrix4fv(location, false, matrix.toFloatBuffer());
