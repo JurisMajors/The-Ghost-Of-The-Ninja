e542e9633d43dce05bffa87e22200e7a3798f869
Juris Majors <jurismajors@gmail.com>
2019-05-04 10:03:47 +0200

    removed some cli arguments

--- src/group4/AI/Evolver.java
+++ src/group4/AI/Evolver.java
@@ -15,26 +15,36 @@ import java.util.Random;
 
 /**
  * Does the process of evolving and outputs the best fitting network.
- * command-line arguments- popSize, elitism, generationCount, maxExpectedFitness, filePath
- * popSize = size of the population within a generation
- * elitism = ...
- * generationCount = how many generations until termination
- * maxExpectedFitness = -1 or the max achievable score. -1 == "I HAVE NO IDEA"
+ *
+ * command-line arguments - filePath
  * filePath = where to store best model
  */
 public class Evolver {
+    public final static int populationSize = 100;
+    public final static int elitism = (int)(populationSize * 0.2);
+    public final static int genCount = 100;
+    public final static int maxFit = Integer.MAX_VALUE;
+
 
     private static void toFile(Brain b, String filePath) throws IOException {
         b.toFile(filePath);
     }
 
+    private static int[] getLayerSizes(String arg) {
+        return null;
+    }
+
     public static void main(String[] args) {
+        String path = args[0];
+
         List<EvolutionaryOperator<Brain>> operators = new LinkedList<>();
         operators.add(new BrainCrossover());
         operators.add(new BrainMutation());
 
+        // put them in a pipeline
         EvolutionaryOperator<Brain> pipeline = new EvolutionPipeline<>(operators);
 
+        // factory of neural networks
         AbstractCandidateFactory<Brain> factory = new BrainFactory();
         FitnessEvaluator<Brain> fitnessEvaluator = new Evaluator<>();
         SelectionStrategy<Object> selection = new RouletteWheelSelection();
@@ -43,19 +53,12 @@ public class Evolver {
         EvolutionEngine<Brain> engine = new GenerationalEvolutionEngine<>(factory, pipeline,
                 fitnessEvaluator, selection, rng);
 
-        int popSize = Integer.parseInt(args[0]);
-        int elitism = Integer.parseInt(args[1]);
-        int genCount = Integer.parseInt(args[2]);
-        int tmpfit = Integer.parseInt(args[3]);
-        String path = args[4];
-
-        int maxFit = tmpfit == -1 ? Integer.MAX_VALUE : tmpfit;
 
         // uncomment to run
 
-        Brain result = engine.evolve(popSize, elitism,
-                new GenerationCount(genCount),
-                new TargetFitness(maxFit, true));
+        Brain result = engine.evolve(Evolver.populationSize, Evolver.elitism,
+                new GenerationCount(Evolver.genCount),
+                new TargetFitness(Evolver.maxFit, true));
 
         // uncomment to save resulting weights
         // Evolver.toFile(result, path);
