bb2d5d3200275eae6759b486ebba5c1e622815c1
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-05-08 21:49:11 +0200

    started working on platform

---
+++ src/group4/ECS/entities/BasicPlatform.java
@@ -0,0 +1,14 @@
+package group4.ECS.entities;
+
+import com.badlogic.ashley.core.Entity;
+import group4.graphics.Texture;
+import group4.maths.Vector3f;
+
+public class BasicPlatform extends Entity {
+
+
+    public BasicPlatform(Vector3f position, Vector3f size, Texture texture, Vector3f textureSize, boolean collidable) {
+
+    }
+
+}
