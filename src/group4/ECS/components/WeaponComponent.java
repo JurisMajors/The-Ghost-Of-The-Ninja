6300d5318bce609e545cda090b344255c499b0c8
nstylo <niklas@stylianou.info>
2019-05-09 19:13:53 +0200

    bullet type as enum (experimental)

--- src/group4/ECS/components/WeaponComponent.java
+++ src/group4/ECS/components/WeaponComponent.java
@@ -1,12 +1,18 @@
 package group4.ECS.components;
 
 import com.badlogic.ashley.core.Component;
+import group4.ECS.etc.EntityConst;
 
 public class WeaponComponent implements Component {
 
     public int damage;
+    public int fireRate;
+    public EntityConst.BulletType type;
 
-    public WeaponComponent(int damage) {
+    public WeaponComponent(int damage, int fireRate, EntityConst.BulletType type) {
         this.damage = damage;
+        this.fireRate = fireRate;
+        this.type = type;
     }
+
 }
--- src/group4/ECS/entities/items/weapons/AK47.java
+++ src/group4/ECS/entities/items/weapons/AK47.java
@@ -1,11 +1,8 @@
 package group4.ECS.entities.items.weapons;
 
-import com.badlogic.ashley.core.Entity;
 import group4.ECS.components.GraphicsComponent;
-import group4.ECS.components.PositionComponent;
 import group4.ECS.components.WeaponComponent;
-import group4.ECS.etc.TheEngine;
-import group4.graphics.Texture;
+import group4.ECS.etc.EntityConst;
 import group4.maths.Vector3f;
 
 public class AK47 extends Gun {
@@ -13,7 +10,7 @@ public class AK47 extends Gun {
     public AK47(Vector3f p, String shader) {
         super(p);
 
-        // AK47 texture
+        // AK47 texture (64 x 32)
         String texture = "src/group4/res/textures/weapons/AK47.png";
 
         // add needed components
@@ -21,7 +18,7 @@ public class AK47 extends Gun {
                 this.vertices, this.indices, this.tcs));
 
         // TODO: damage-system
-        this.add(new WeaponComponent(10));
+        this.add(new WeaponComponent(10, 600, EntityConst.BulletType.MACHINEGUN));
 
     }
 
