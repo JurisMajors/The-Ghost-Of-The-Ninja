76109e6fda0a4d33d25e049efd53b27627d5ceb3
v777v <38751851+v777v@users.noreply.github.com>
2019-05-10 13:26:38 +0200

    Position+range component

--- src/group4/ECS/components/PositionComponent.java
+++ src/group4/ECS/components/PositionComponent.java
@@ -7,6 +7,12 @@ public class PositionComponent implements Component {
     public Vector3f position;
     public Vector3f dimension;
 
+    /**
+     * Creates a position component
+     *
+     * @param position  center point of player
+     * @param dimension along with the position describes a cuboid with lbbCorner=position-dimention and rtfCorner=position+dimention
+     */
     public PositionComponent(Vector3f position, Vector3f dimension) {
         this.position = position;
         this.dimension = dimension;
