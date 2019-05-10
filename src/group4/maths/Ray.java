462e505ceb1f92e0bb288f8c8acec8766de67d1d
Juris Majors <jurismajors@gmail.com>
2019-05-10 11:52:26 +0200

    ray

---
+++ src/group4/maths/Ray.java
@@ -0,0 +1,14 @@
+package group4.maths;
+
+public class Ray {
+    /** starting position of the ray **/
+    Vector3f startPos;
+
+    public Ray(Vector3f startingPos) {
+        this.startPos = startingPos;
+    }
+
+    Vector3f cast() {
+        return null;
+    }
+}
