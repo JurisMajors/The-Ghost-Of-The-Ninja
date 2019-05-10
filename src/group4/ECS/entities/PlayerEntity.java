5f7a5cbbac93726d30eb97682f0742cc4b2f78f8
v777v <38751851+v777v@users.noreply.github.com>
2019-05-10 13:09:05 +0200

    PlayerEntity

--- src/group4/ECS/entities/PlayerEntity.java
+++ src/group4/ECS/entities/PlayerEntity.java
@@ -7,9 +7,19 @@ import group4.maths.Vector3f;
 
 public class PlayerEntity extends Entity {
 
-    public PlayerEntity(Vector3f position, Vector3f dimension, Vector3f velocity, Vector3f velocityRange, Vector3f acceleration, Vector3f gravity){
+    /**
+     * Creates a player
+     *
+     * @param position      center point of player
+     * @param dimension     along with the position describes a cuboid with lbbCorner=position-dimention and rtfCorner=position+dimention
+     * @param velocity      velocity vector of player
+     * @param velocityRange restricting the velocity: -velocityRange.x<=velocity.x<=velocityRange.x and velocity.y<=velocityRange.y
+     * @param acceleration  acceleration vector of player
+     * @param gravity       acceleration vector of player due to gravity
+     */
+    public PlayerEntity(Vector3f position, Vector3f dimension, Vector3f velocity, Vector3f velocityRange, Vector3f acceleration, Vector3f gravity) {
         this.add(new PositionComponent(position, dimension));
-        this.add(new MovementComponent(velocity, velocityRange,acceleration));
+        this.add(new MovementComponent(velocity, velocityRange, acceleration));
         this.add(new GravityComponent(gravity));
         this.add(new PlayerComponent());
         TheEngine.getInstance().addEntity(this);
