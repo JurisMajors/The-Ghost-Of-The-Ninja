b55599e58bedb84837d5972dca0089b3df65eb32
v777v <38751851+v777v@users.noreply.github.com>
2019-05-10 13:36:46 +0200

    Families

--- src/group4/ECS/etc/Families.java
+++ src/group4/ECS/etc/Families.java
@@ -9,11 +9,24 @@ import group4.ECS.components.*;
  */
 public class Families {
 
+    //All platforms
     public static final Family platformFamily = Family.all(PlatformComponent.class).get();
+
+    //All moving platforms
     public static final Family movingPlatformFamily = Family.all(MovementComponent.class, PlatformComponent.class).get();
+
+    //All moving entities except platforms
     public static final Family movingNonPlatformFamily = Family.all(MovementComponent.class).exclude(PlatformComponent.class).get();
-    public static final Family movingGravityMobFamily = Family.all(MovementComponent.class, MobComponent.class).get();
+
+    //All moving mobs except flying mobs
+    public static final Family movingGravityMobFamily = Family.all(GravityComponent.class, MovementComponent.class, MobComponent.class).get();
+
+    //All flying mobs
     public static final Family movingNonGravityMobFamily = Family.all(MovementComponent.class, MobComponent.class).exclude(GravityComponent.class).get();
+
+    //All (moving) player(s)
     public static final Family playerFamily = Family.all(MovementComponent.class, PlayerComponent.class).get();
+
+    //All entities (with a position)
     public static final Family allFamily = Family.all(PositionComponent.class).get();
 }
