a930b115aa53e8ae6e9a197348970676c1f2d28e
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-05-11 16:05:31 +0200

    added basic graphics to mobs

--- src/group4/ECS/entities/mobs/WalkingMob.java
+++ src/group4/ECS/entities/mobs/WalkingMob.java
@@ -1,11 +1,13 @@
 package group4.ECS.entities.mobs;
 
 import group4.ECS.components.GravityComponent;
+import group4.graphics.Shader;
+import group4.graphics.Texture;
 import group4.maths.Vector3f;
 
-public class WalkingMob extends Mob{
+public class WalkingMob extends Mob {
 
-     /**
+    /**
      * Creates a walking mob (a mob affected by gravity)
      *
      * @param position      left-bottom-back corner of the cuboid representing the mob
@@ -15,9 +17,9 @@ public class WalkingMob extends Mob{
      * @param acceleration  acceleration vector of mob
      * @param gravity       acceleration vector of mob due to gravity
      */
-    public WalkingMob(Vector3f position, Vector3f dimension, Vector3f velocity, Vector3f velocityRange, Vector3f acceleration, Vector3f gravity) {
+    public WalkingMob(Vector3f position, Vector3f dimension, Vector3f velocity, Vector3f velocityRange, Vector3f acceleration, Vector3f gravity, Shader shader, Texture texture) {
         // setup basic mob and add it to the engine
-        super(position, dimension, velocity, velocityRange, acceleration);
+        super(position, dimension, velocity, velocityRange, acceleration, shader, texture);
 
         // add gravity to the mob
         this.add(new GravityComponent(gravity));
