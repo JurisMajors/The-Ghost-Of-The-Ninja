a5b83c22853c6bfa787acf0bd7b2c0c2e9de099c
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-13 14:32:38 +0200

    Added comments

--- src/group4/utils/DebugUtils.java
+++ src/group4/utils/DebugUtils.java
@@ -9,10 +9,7 @@ public class DebugUtils {
     private static Vector3f color = new Vector3f(1.0f, 0.0f, 0.0f); // RED
 
     // nobody should create an object of this class
-    private DebugUtils() {
-    }
-
-    ;
+    private DebugUtils() {}
 
     /**
      * Given two vector positions, draws a red line between them using old style OpenGL.
@@ -22,13 +19,19 @@ public class DebugUtils {
      */
     public static void drawLine(Vector3f a, Vector3f b) {
         glLineWidth(lineWidth);
-        glColor3f(1.0f, 0.0f, 0.0f);
+        glColor3f(color.x, color.y, color.z);
         glBegin(GL_LINES);
         glVertex2f(a.x, a.y);
         glVertex2f(b.x, b.y);
         glEnd();
     }
 
+    /**
+     * Given two opposite corner positions, draws a red lined bbox between them using old style OpenGL.
+     *
+     * @param bottomLeft Vector3f, first corner of the bbox to draw
+     * @param topRight Vector3f, second corner of the bbox to draw
+     */
     public static void drawBox(Vector3f bottomLeft, Vector3f topRight) {
         glLineWidth(lineWidth);
         glColor3f(color.x, color.y, color.z);
@@ -40,6 +43,12 @@ public class DebugUtils {
         glEnd();
     }
 
+    /**
+     * Draws a grid where the grid cells are square and of the given cellSize. Utilizes
+     * the drawLine function for drawing large vertical and horizontal lines.
+     *
+     * @param cellSize Float, the length of the side a square grid cell should be
+     */
     public static void drawGrid(float cellSize) {
         for (int i = -100; i < 100; i++) {
             // Draw vertical
