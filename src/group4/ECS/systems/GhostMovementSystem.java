6cb8203c335ced793fcbe3c56c268285a0eb29d5
Juris Majors <j.a.majors@student.tue.nl>
2019-05-13 13:56:25 +0000

    ghost movement system

---
+++ src/group4/ECS/systems/GhostMovementSystem.java
@@ -0,0 +1,34 @@
+package group4.ECS.systems;
+
+import com.badlogic.ashley.core.Entity;
+import group4.AI.GhostMove;
+import group4.ECS.components.GhostComponent;
+import group4.ECS.etc.Families;
+
+public class GhostMovementSystem extends PlayerMovementSystem {
+
+    public GhostMovementSystem() {
+        super(Families.ghostFamily) ;
+    }
+
+    @Override
+    protected boolean shouldLeft(Object ref) {
+        return GhostMove.LEFT.equals((Integer) ref);
+    }
+
+    @Override
+    protected boolean shouldRight(Object ref) {
+        return GhostMove.RIGHT.equals((Integer) ref);
+    }
+
+    @Override
+    protected Object getMovementRef(Entity e) {
+        GhostComponent ghostComp = e.getComponent(GhostComponent.class);
+        return ghostComp.brain.think();
+    }
+
+    @Override
+    protected boolean shouldJump(Object ref) {
+        return GhostMove.JUMP.equals((Integer) ref);
+    }
+}
