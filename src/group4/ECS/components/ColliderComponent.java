19d06b3948eaea5b534582e8a83680f2baef3e94
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-14 17:09:49 +0200

    Removed the assumption that dimension => collision

---
+++ src/group4/ECS/components/ColliderComponent.java
@@ -0,0 +1,12 @@
+package group4.ECS.components;
+
+import com.badlogic.ashley.core.Component;
+import group4.maths.Vector3f;
+
+/**
+ * DimensionComponent aka Bounding Box, will be needed for Damage, Collision, AI
+ */
+public class ColliderComponent implements Component {
+    public ColliderComponent() {}
+
+}
