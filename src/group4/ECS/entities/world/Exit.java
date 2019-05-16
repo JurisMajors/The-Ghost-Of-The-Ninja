b2981570ae476902597f6d6699788fbd20052375
Olivier <38719323+20173586@users.noreply.github.com>
2019-05-16 16:52:14 +0200

    Start of exit overlap/collision detection

--- src/group4/ECS/entities/world/Exit.java
+++ src/group4/ECS/entities/world/Exit.java
@@ -1,9 +1,8 @@
 package group4.ECS.entities.world;
 
 import com.badlogic.ashley.core.Entity;
-import group4.ECS.components.DimensionComponent;
-import group4.ECS.components.GraphicsComponent;
-import group4.ECS.components.PositionComponent;
+import group4.ECS.components.*;
+import group4.ECS.systems.collision.CollisionHandlers.ExitCollision;
 import group4.graphics.Shader;
 import group4.graphics.Texture;
 import group4.levelSystem.Module;
@@ -11,18 +10,24 @@ import group4.maths.Vector3f;
 
 public class Exit extends Entity {
 
-    Module module;
+    public Module module;
 
     /**
      * Construct a simple block (flexible texture and shader) in a certain position of certain size
      *
      * @param p       The position of the block (lower left corner)
      * @param d       The dimensions of the block
+     * @param m       The module the exit is part of
      */
     public Exit(Vector3f p, Vector3f d, Module m) {
         this.add(new PositionComponent(p));
         this.add(new DimensionComponent(d));
         this.add(new GraphicsComponent(Shader.SIMPLE, Texture.EXIT, d));
+        // TODO: one of these should be redundant and removed
+        this.add(new CollisionComponent(ExitCollision.getInstance()));
+        this.add(new ColliderComponent());
+
+        // Set the module this exit belongs to
         this.module = m;
     }
 
---
+++ src/group4/ECS/systems/collision/CollisionHandlers/ExitCollision.java
@@ -0,0 +1,19 @@
+package group4.ECS.systems.collision.CollisionHandlers;
+
+import group4.ECS.components.CollisionComponent;
+import group4.ECS.entities.world.Exit;
+
+public class ExitCollision extends AbstractCollisionHandler<Exit> {
+
+    /** Singleton **/
+    private static AbstractCollisionHandler me = new BlockCollision();
+
+    @Override
+    public void collision(Exit e, CollisionComponent cc) {
+        System.out.println("Reached exit");
+    }
+
+    public static AbstractCollisionHandler getInstance() {
+        return me;
+    }
+}
