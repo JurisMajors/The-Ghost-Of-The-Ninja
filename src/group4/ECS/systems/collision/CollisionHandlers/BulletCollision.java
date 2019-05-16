713d8d531fd6a042fa859fcf00bbb7720c342f9f
Juris Majors <j.a.majors@student.tue.nl>
2019-05-16 11:33:59 +0000

    delete bullet on collision

--- src/group4/ECS/systems/collision/CollisionHandlers/BulletCollision.java
+++ src/group4/ECS/systems/collision/CollisionHandlers/BulletCollision.java
@@ -2,6 +2,7 @@ package group4.ECS.systems.collision.CollisionHandlers;
 
 import group4.ECS.components.CollisionComponent;
 import group4.ECS.entities.items.weapons.Bullet;
+import group4.ECS.etc.TheEngine;
 
 public class BulletCollision extends AbstractCollisionHandler<Bullet> {
 
@@ -10,7 +11,16 @@ public class BulletCollision extends AbstractCollisionHandler<Bullet> {
 
     @Override
     public void collision(Bullet e, CollisionComponent cc) {
-
+        if (!cc.collisions.isEmpty()) {
+            // if has collided with something remove it from the engine
+            // this still allows to process knockback
+            // and damage for other entities, since its stored within the
+            // set of collisions in the other entity
+            TheEngine.getInstance().removeEntity(e);
+            // clear the set so that we
+            // dont do displacement of the bullet in the UncollidingSystem
+            cc.collisions.clear();
+        }
     }
 
     public static AbstractCollisionHandler getInstance() {
