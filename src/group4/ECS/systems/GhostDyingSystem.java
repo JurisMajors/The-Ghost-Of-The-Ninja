ea8cfae244615d195280e696d758d453e850658b
Juris Majors <j.a.majors@student.tue.nl>
2019-05-17 13:17:01 +0000

    reset to start

--- src/group4/ECS/systems/GhostDyingSystem.java
+++ src/group4/ECS/systems/GhostDyingSystem.java
@@ -1,5 +1,8 @@
 package group4.ECS.systems;
 
+import com.badlogic.ashley.core.Entity;
+import group4.ECS.components.PositionComponent;
+import group4.ECS.entities.Ghost;
 import group4.ECS.etc.Families;
 
 public class GhostDyingSystem extends PlayerDyingSystem {
@@ -12,4 +15,11 @@ public class GhostDyingSystem extends PlayerDyingSystem {
         super(Families.ghostFamily, false);
     }
 
+
+    @Override
+    protected boolean die(Entity entity, float deltaTime) {
+        Ghost g = (Ghost) entity;
+        g.getComponent(PositionComponent.class).position = g.level.getCurrentModule().getPlayerInitialPosition();
+        return false;
+    }
 }
