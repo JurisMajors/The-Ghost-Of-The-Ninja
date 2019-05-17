0a8f76aed77f6fca6aec1e864c8530b21e455d1f
Juris Majors <j.a.majors@student.tue.nl>
2019-05-17 12:38:57 +0000

    velocity fix and earlier deatha dded

--- src/group4/ECS/systems/GhostDyingSystem.java
+++ src/group4/ECS/systems/GhostDyingSystem.java
@@ -1,5 +1,8 @@
 package group4.ECS.systems;
 
+import com.badlogic.ashley.core.Entity;
+import group4.AI.GhostMove;
+import group4.ECS.components.GhostComponent;
 import group4.ECS.etc.Families;
 
 public class GhostDyingSystem extends PlayerDyingSystem {
@@ -12,4 +15,21 @@ public class GhostDyingSystem extends PlayerDyingSystem {
         super(Families.ghostFamily, false);
     }
 
+    @Override
+    protected boolean shouldDie(Entity entity, float deltaTime) {
+        int[] moveFreq = entity.getComponent(GhostComponent.class).moveFreq;
+        boolean tooDumb = false;
+        int argMax = 0;
+        int zerosCount = 0;
+        for (int i = 0; i < moveFreq.length ; i++) {
+            if (moveFreq[i] > moveFreq[argMax]) {
+                argMax = i;
+            }
+            if (moveFreq[i] == 0) {
+                zerosCount ++;
+            }
+        }
+        tooDumb = (moveFreq[argMax] > 50 && zerosCount == 2);
+        return super.shouldDie(entity, deltaTime) || tooDumb;
+    }
 }
