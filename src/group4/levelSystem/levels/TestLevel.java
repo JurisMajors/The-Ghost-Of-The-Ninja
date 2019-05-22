0ab141058235ddec313c246a962b35c04d9ddb1f
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-05-22 13:13:11 +0200

    added a mob to the test level so I can actually test them in the test level

--- src/group4/levelSystem/levels/TestLevel.java
+++ src/group4/levelSystem/levels/TestLevel.java
@@ -1,6 +1,8 @@
 package group4.levelSystem.levels;
 
 import group4.ECS.entities.Player;
+import group4.ECS.entities.mobs.ShootingJumpingWalkingMob;
+import group4.ECS.etc.TheEngine;
 import group4.levelSystem.ExitAction;
 import group4.levelSystem.Level;
 import group4.levelSystem.Module;
@@ -23,6 +25,9 @@ public class TestLevel extends Level {
 
     @Override
     protected Player createPlayer() {
+        //temporary add mob to the level here
+        TheEngine.getInstance().addEntity(new ShootingJumpingWalkingMob(new Vector3f(15.0f, 10.0f, 0.0f), this));
+
         // We don't care about the player position, as that will be initialized on level switching
         return new Player(new Vector3f(), this);
     }
