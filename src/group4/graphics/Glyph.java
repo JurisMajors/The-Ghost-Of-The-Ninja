e97c3600e53f9ff2afcb0ec392a8071f673a282a
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-24 00:48:03 +0200

    Added the advance parameter

--- src/group4/graphics/Glyph.java
+++ src/group4/graphics/Glyph.java
@@ -3,6 +3,7 @@ package group4.graphics;
 public class Glyph {
     public final int width, height;
     public final int x, y;
+    public final float advance;
 
     /**
      * Glyph constructor for storing the exact location of a glyph on an atlas containing
@@ -12,11 +13,13 @@ public class Glyph {
      * @param height Integer, height of the glyph
      * @param x Integer, x position on the atlas
      * @param y Integer, y position on the atlas
+     * @param advance Float, value which defines the character spacing
      */
-    public Glyph(int width, int height, int x, int y) {
+    public Glyph(int width, int height, int x, int y, float advance) {
         this.width = width;
         this.height = height;
         this.x = x;
         this.y = y;
+        this.advance = advance;
     }
 }
