b9a6b875e6d0e63b9fc7c3d68d17889e8535f162
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-24 00:28:59 +0200

    Load new level

--- src/group4/game/Main.java
+++ src/group4/game/Main.java
@@ -24,6 +24,7 @@ import group4.input.KeyBoard;
 import group4.input.MouseClicks;
 import group4.input.MouseMovement;
 import group4.levelSystem.Level;
+import group4.levelSystem.levels.Level01;
 import group4.levelSystem.levels.MobTestLevel;
 import group4.levelSystem.levels.TestLevel;
 import group4.levelSystem.levels.TiledLevel;
@@ -142,7 +143,7 @@ public class Main implements Runnable {
             // Systems which are essentially observers of the changed gamestate
             engine.addSystem(new CameraSystem(Families.playerFamily)); // CameraSystem must be added BEFORE RenderSystem
             engine.addSystem(new RenderSystem());
-            this.level = new TestLevel();
+            this.level = new Level01();
 
         }
         // Initialize the level
