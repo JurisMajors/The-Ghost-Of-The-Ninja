c2ad96b2abf0778c07c80ed152cc2d0fc81463d0
Nico Klaassen <klaassen.nico@gmail.com>
2019-05-24 11:11:19 +0200

    Default exits to stop build complaints

--- src/group4/levelSystem/levels/Level01.java
+++ src/group4/levelSystem/levels/Level01.java
@@ -2,6 +2,7 @@ package group4.levelSystem.levels;
 
 import group4.ECS.entities.Player;
 import group4.ECS.entities.mobs.ShootingJumpingWalkingMob;
+import group4.ECS.entities.world.Exit;
 import group4.ECS.etc.TheEngine;
 import group4.levelSystem.ExitAction;
 import group4.levelSystem.Level;
@@ -24,25 +25,25 @@ public class Level01 extends Level {
 
     @Override
     protected Player createPlayer() {
-        //temporary add mob to the level here
-        TheEngine.getInstance().addEntity(new ShootingJumpingWalkingMob(new Vector3f(15.0f, 10.0f, 0.0f), this));
-
         // We don't care about the player position, as that will be initialized on level switching
         return new Player(new Vector3f(), this);
     }
 
     @Override
     protected void configExits() {
-        // Configure the only exit of the level: the exit of the test module
-        this.setExitAction(
-                this.modules.get(0).getExits().get(0), // The exit that we want to set the action for
-                new ExitAction(this) { // Make sure to pass in the level in the constructor, so we can call back to it
-                    @Override
-                    public void exit() { // The actual action to execute
-                        this.callBackLevel.switchModule(this.callBackLevel.getModuleReference(1)); // Switch to the Simple Module once the exit is reached
-                    }
-                }
-        );
+        // We need to configure all exits
+        ExitAction global = new ExitAction() {
+            @Override
+            public void exit() {
+                System.out.println("Reached the exit :-)");
+            }
+        };
+
+        for (Module m : this.modules) {
+            for (Exit e : m.getExits()) {
+                this.setExitAction(e, global);
+            }
+        }
     }
 
 }
