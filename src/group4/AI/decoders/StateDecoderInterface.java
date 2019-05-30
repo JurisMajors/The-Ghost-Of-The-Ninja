774790969cf5b2fb1a9cae38b309d0b426613218
Juris Majors <j.a.majors@student.tue.nl>
2019-05-30 01:06:06 +0000

    setting init and loading definition

--- src/group4/AI/decoders/StateDecoderInterface.java
+++ src/group4/AI/decoders/StateDecoderInterface.java
@@ -1,5 +1,6 @@
 package group4.AI.decoders;
 
+import org.json.JSONObject;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
 /**
@@ -20,4 +21,16 @@ public interface StateDecoderInterface {
      * decode(AnyGameState).length == getInputSize()
      */
     int getInputSize();
+
+    /**
+     * Write the settings of this decoder to a json object
+     * @return json object representing the settings of the decoder instance
+     */
+    JSONObject getSettings();
+
+    /**
+     * Initialize based on given settings
+     * @param settings jsonObject of settings to load in this decoder
+     */
+    void loadOnSettings(JSONObject settings);
 }
