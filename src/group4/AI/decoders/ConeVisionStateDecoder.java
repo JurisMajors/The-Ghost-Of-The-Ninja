baf0eb5fed3bfdf072cd848a3121b2c1e5851e06
Juris Majors <j.a.majors@student.tue.nl>
2019-05-30 01:10:28 +0000

    static loaders

--- src/group4/AI/decoders/ConeVisionStateDecoder.java
+++ src/group4/AI/decoders/ConeVisionStateDecoder.java
@@ -3,6 +3,7 @@ package group4.AI.decoders;
 import com.badlogic.ashley.core.Entity;
 import group4.ECS.components.stats.MovementComponent;
 import group4.maths.Vector3f;
+import org.json.JSONObject;
 
 /**
  * Starts the ray casting on an angle, relative to the velocity of the ghost
@@ -19,5 +20,18 @@ public class ConeVisionStateDecoder extends RayStateDecoder{
         Vector3f start = ghostVel.rotateXY(-1 * this.angleRange / 2.0f);
         start.normalizei();
         return start;
+    }
+
+    @Override
+    public JSONObject getSettings() {
+        return null;
+    }
+
+    /**
+     * Initialize based on given settings
+     * @param settings jsonObject of settings to load in this decoder
+     */
+    public static void loadOnSettings(JSONObject settings) {
+
     }
 }
