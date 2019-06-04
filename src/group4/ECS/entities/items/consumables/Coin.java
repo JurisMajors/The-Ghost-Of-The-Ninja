73e2927268fa6c593ab7766fc22c7a25ecf8abf3
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-06-04 11:22:48 +0200

    coin entity

--- src/group4/ECS/entities/items/consumables/Coin.java
+++ src/group4/ECS/entities/items/consumables/Coin.java
@@ -1,4 +1,18 @@
 package group4.ECS.entities.items.consumables;
 
-public class Coin {
+import group4.ECS.components.identities.CoinComponent;
+import group4.ECS.components.physics.CollisionComponent;
+import group4.ECS.entities.items.Item;
+
+public class Coin extends Item {
+
+    /**
+     * Creates a coin with score value value.
+     * @param value score value
+     */
+    public Coin(int value) {
+        this.add(new CoinComponent(value));
+        this.add(new CollisionComponent());
+    }
+
 }
