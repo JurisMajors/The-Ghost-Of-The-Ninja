9cf8202913965fdd12c37cb4fd817df935bc5a80
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-06-04 13:22:38 +0200

    added a useless collision handler

--- src/group4/ECS/systems/collision/CollisionHandlers/CoinCollision.java
+++ src/group4/ECS/systems/collision/CollisionHandlers/CoinCollision.java
@@ -1,13 +1,7 @@
 package group4.ECS.systems.collision.CollisionHandlers;
 
-import group4.ECS.components.identities.CoinComponent;
 import group4.ECS.components.physics.CollisionComponent;
-import group4.ECS.components.stats.ScoreComponent;
-import group4.ECS.entities.Ghost;
-import group4.ECS.entities.Player;
 import group4.ECS.entities.items.consumables.Coin;
-import group4.ECS.etc.Mappers;
-import group4.ECS.systems.collision.CollisionData;
 
 public class CoinCollision extends AbstractCollisionHandler<Coin> {
 
@@ -18,19 +12,7 @@ public class CoinCollision extends AbstractCollisionHandler<Coin> {
 
     @Override
     public void collision(Coin e, CollisionComponent cc) {
-        for (CollisionData cd : cc.collisions) {
-            // only handle collision with coin and player
-            if (cd.entity instanceof Player && !(cd.entity instanceof Ghost)) {
-                ScoreComponent playerScore = Mappers.scoreMapper.get(cd.entity);
-                CoinComponent coinComponent = Mappers.coinMapper.get(e);
-
-                // update score
-                playerScore.addScore(coinComponent.value);
-
-                // remove coin from the module
-                ((Player) cd.entity).level.getCurrentModule().removeEntity(e);
-            }
-        }
+        cc.collisions.clear();
     }
 
     public static AbstractCollisionHandler getInstance() {
