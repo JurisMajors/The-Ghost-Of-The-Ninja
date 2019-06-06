46ef35e1204a07933d650f91f5113709e2ac4c70
Nico Klaassen <klaassen.nico@gmail.com>
2019-06-06 23:31:25 +0200

    Renderlayer is now a proper enum...

--- src/group4/graphics/RenderLayer.java
+++ src/group4/graphics/RenderLayer.java
@@ -1,10 +1,8 @@
 package group4.graphics;
 
-public class RenderLayer {
-    public enum Layer {
-        BACKGROUND,
-        MAIN,
-        FOREGROUND,
-        FX
-    }
+public enum RenderLayer {
+    BACKGROUND,
+    MAIN,
+    FOREGROUND,
+    FX
 }
