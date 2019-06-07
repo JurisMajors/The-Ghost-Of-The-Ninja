6ded2dd7e2c2ba2102b9e2a59b8cae4f02c3f0ec
Juris Majors <jurismajors@gmail.com>
2019-06-07 12:18:47 +0200

    totem entity

---
+++ src/group4/ECS/entities/Totem.java
@@ -0,0 +1,51 @@
+package group4.ECS.entities;
+
+import com.badlogic.ashley.core.Entity;
+import group4.ECS.components.GraphicsComponent;
+import group4.ECS.components.physics.CollisionComponent;
+import group4.ECS.components.physics.DimensionComponent;
+import group4.ECS.components.physics.PositionComponent;
+import group4.ECS.systems.collision.CollisionHandlers.TotemCollision;
+import group4.graphics.Shader;
+import group4.graphics.Texture;
+import group4.levelSystem.Level;
+import group4.maths.Vector3f;
+
+public class Totem extends Entity {
+
+    private Vector3f dimension = new Vector3f(1.0f, 2f,0);
+
+    private String name;
+
+    private Ghost ghost;
+
+    public Level level;
+
+    Totem(Vector3f position, String name, String ghostDir, Level level) {
+        this.name = name;
+        this.level = level;
+        this.setGhost(ghostDir);
+        this.add(new PositionComponent(position));
+        this.add(new DimensionComponent(dimension));
+        // TODO: add collision component
+        this.add(new CollisionComponent(TotemCollision.getInstance()));
+        this.add(new GraphicsComponent(Shader.SIMPLE, Texture.DEBUG, dimension, false));
+    }
+
+    private void setGhost(String dir) {
+        this.ghost = new Ghost(this.getComponent(PositionComponent.class).position,
+                this.level, dir + getID());
+    }
+
+    public Ghost getGhost() {
+        return this.ghost;
+    }
+
+    public boolean isEnd() {
+        return this.name.charAt(0) == 'e';
+    }
+
+    public int getID() {
+        return Integer.parseInt(this.name.substring(1));
+    }
+}
