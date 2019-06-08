95108b95575c66169b935eda8608fe90435725b3
Juris Majors <j.a.majors@student.tue.nl>
2019-06-08 14:19:11 +0200

    Loading new sounds and play random functionality

--- src/group4/audio/Sound.java
+++ src/group4/audio/Sound.java
@@ -5,6 +5,7 @@ import org.lwjgl.system.MemoryStack;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.lwjgl.openal.AL10.*;
 import static org.lwjgl.stb.STBVorbis.stb_vorbis_decode_filename;
@@ -23,15 +24,22 @@ public class Sound {
      */
     private int sourcePointer;
 
+    private static Random ran = new Random();
+
     /**
      * List of all sounds in the game.
      */
     public static Sound BACKGROUND;
     public static Sound BEEP;
-    public static Sound SLASH;
+    public static Sound[] SLASH;
+    public static Sound SLASH1;
+    public static Sound SLASH2;
     public static Sound COIN;
     public static Sound DIE;
-    public static Sound MOBDIE;
+    public static Sound MOBHIT;
+    public static Sound MOBDIE1;
+    public static Sound MOBDIE2;
+    public static Sound[] MOBDIE;
     public static Sound MENU;
 
     private static ArrayList<Sound> allSounds;
@@ -44,18 +52,32 @@ public class Sound {
 
         BACKGROUND = new Sound("background.ogg", true, 0.5f);
         BEEP = new Sound("fire.ogg", false);
-        SLASH = new Sound("slash.ogg", false);
+
+        SLASH1 = new Sound("slash1.ogg", false);
+        SLASH2 = new Sound("slash2.ogg", false);
+        SLASH = new Sound[]{SLASH1, SLASH2};
+
         COIN = new Sound("coin.ogg", false);
+
         DIE = new Sound("die.ogg", false);
-        MOBDIE = new Sound("mobdie.ogg", false);
-        MENU = new Sound("menu.ogg", false);
+        MOBDIE1 = new Sound("mobdie1.ogg", false);
+        MOBDIE2 = new Sound("mobdie1.ogg", false);
+        MOBDIE = new Sound[]{MOBDIE1, MOBDIE2};
+
+        MOBHIT = new Sound("mobhit.ogg", false);
+
+        MENU = new Sound("menu.ogg", false, 0.7f);
 
         allSounds.add(BACKGROUND);
         allSounds.add(BEEP);
-        allSounds.add(SLASH);
+        allSounds.add(SLASH1);
+        allSounds.add(SLASH2);
         allSounds.add(COIN);
         allSounds.add(DIE);
-        allSounds.add(MOBDIE);
+        allSounds.add(MOBDIE1);
+        allSounds.add(MOBDIE2);
+        allSounds.add(MOBHIT);
+        allSounds.add(MENU);
     }
 
     /**
@@ -99,6 +121,10 @@ public class Sound {
     public void play() {
         //Play the sound
         alSourcePlay(sourcePointer);
+    }
+
+    public static void playRandom(Sound... sounds) {
+        sounds[ran.nextInt(sounds.length)].play();
     }
 
     /**
