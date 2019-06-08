f6edb161e7000743810aabaccdc0e4495f071d77
Nico <klaassen.nico@gmail.com>
2019-06-08 14:21:56 +0200

    Updates

--- src/group4/res/maps/tilemap-main.tsx
+++ src/group4/res/maps/tilemap-main.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tilemap-main" tilewidth="32" tileheight="32" tilecount="72" columns="8">
- <image source="../textures/tilemap-main.png" width="256" height="288"/>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset-main" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="../textures/tilemap-main.png" width="512" height="384"/>
 </tileset>
