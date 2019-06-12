7cc6567a05d751f5048371fb593d0df873c3b587
JorisOnGithub <joris.d.goddijn@gmail.com>
2019-06-12 12:22:23 +0200

    resolved imports after merging

--- src/group4/ECS/etc/Families.java
+++ src/group4/ECS/etc/Families.java
@@ -2,12 +2,6 @@ package group4.ECS.etc;
 
 import com.badlogic.ashley.core.Family;
 import group4.ECS.components.*;
-import group4.ECS.components.events.TickComponent;
-import group4.ECS.components.events.TimedComponent;
-import group4.ECS.components.AudioComponent;
-import group4.ECS.components.GraphicsComponent;
-import group4.ECS.components.SplineComponent;
-import group4.ECS.components.SplinePathComponent;
 import group4.ECS.components.events.EventComponent;
 import group4.ECS.components.identities.*;
 import group4.ECS.components.physics.CollisionComponent;
@@ -18,7 +12,6 @@ import group4.ECS.components.stats.DamageComponent;
 import group4.ECS.components.stats.HealthComponent;
 import group4.ECS.components.stats.MovementComponent;
 import group4.ECS.components.stats.RangeWeaponComponent;
-import group4.ECS.entities.AStarMobs.AStarMob;
 
 /**
  * This class determines groups (families) of entities which share the same components
